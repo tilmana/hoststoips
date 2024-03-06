1762b2f2d6e91022e20023540405d7faa6a37266
Arthur <59236083+tilmana@users.noreply.github.com>
2024-03-05 19:01:14 -0500

    Update hoststoips.py

--- hoststoips.py
+++ hoststoips.py
@@ -6,21 +6,29 @@ fileName = input("Filename: ")
 file1 = open(fileName, "r")
 file1lines = file1.readlines()
 for line in file1lines:
-    line = line.strip("\n")
-    print(line)
-    if "http://" in line or "https//" in line:
-        line = line.split("//")[0]
-    if line.startswith("//"):
-        line = line[2::]
-    if ":" in line:
-        line = line.split(":")[0]
-    if "/" in line:
-        line = line.split("/")[0]
+  line = line.strip("\n")
+  if "http://" in line or "https//" in line:
+    line = line.split("//")[0]
+  if line.startswith("//"):
+    line = line[2::]
+  if ":" in line:
+    line = line.split(":")[0]
+  if "/" in line:
+    line = line.split("/")[0]
+  try:
     ip = socket.gethostbyname(line)
-    if ip not in records:
-        records[ip] = line
+  except Exception as e:
+    if not "N/A" in records.keys():
+      records["N/A"] = line
     else:
-        records[ip] = records[ip] + ", " + line
+      records["N/A"] = records["N/A"] + ", " + line
+    continue
+  if ip not in records:
+    records[ip] = line
+  else:
+    if line in records[ip].split(", "):
+      continue
+    records[ip] = records[ip] + ", " + line
 
 for k, v in records.items():
-    print("{0}: {1}".format(k, v))
+  print("{0}: {1}".format(k, v))
